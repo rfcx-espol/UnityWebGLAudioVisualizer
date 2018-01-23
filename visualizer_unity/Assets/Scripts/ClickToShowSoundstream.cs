07dafff5b6b6b3d5d82af5e9b30287e2801cd44e
Jorge Blacio <jorgeblacio@gmail.com>
2018-01-23 14:16:08 -0500

    Restructuracion de los archivos y assets del proyecto para que cuando se vaya a trabajar sobre UNity se pueda facilmente encontrar las fuentes de donde sale todo. Se importo un mejor modelo 3D del sensor, aunque este no represente el sensor final. Se agrego un pequeño feedback a los sensores. Se estandarizo el sensor y se hizo un prefab para que sea facil agregar futuros sensores.

---
+++ visualizer_unity/Assets/Scripts/ClickToShowSoundstream.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class ClickToShowSoundstream : MonoBehaviour {
+
+    public ForestInput forest;
+    public RadioMutex mutex;
+    public MeshRenderer[] feedback;
+
+   
+
+    // Use this for initialization
+    void Start () {
+        timer = 0;
+        w_controller = forest.waveCanvas.GetComponent<WavesController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (forest.IsActive)
+        {
+            timer += Time.deltaTime;
+            if (timer >= wait_for_load && loading)
+            {
+                loading = false;
+                w_controller.doStart();
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    for (int i = 0; i < feedback.Length; i++)
+                        feedback[i].material.color = Color.red;
+                }
+            }
+        }
+	}
+
+    void OnMouseDown()
+    {
+        if (!forest.IsActive)
+        {
+            for (int i = 0; i < feedback.Length; i++)
+                feedback[i].material.color = Color.green;
+            forest.EnableVisualizer(mutex.station);
+            loading = true;
+            timer = 0;
+            mutex.do_set_station();          
+        }
+    }
+
+    float timer;
+    bool loading;
+    float wait_for_load = 3.0f;
+    WavesController w_controller;
+}
